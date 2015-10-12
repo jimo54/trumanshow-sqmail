3220f4a8ff670132dad086fd7c8d4da312433393
Jim Owens <jim@26maidenlane.net>
2015-10-12 12:32:48 -0400

    Added comment

--- to_html.sh
+++ to_html.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-for l in `ls /var/lib/squirrelmail/data/*.pref`; 
-do sed -i s/show_html_default=0/show_html_default=1/g $l; 
+#####################################
+#                                   #
+# to_html.sh                        #
+#                                   #
+# This tiny script will change the  #
+# default view in all SquirrelMail  #
+# user accounts on this system to   #
+# html vs plaintext.                #
+#                                   #
+#                                   #
+#####################################
+
+for l in `ls /var/lib/squirrelmail/data/*.pref`
+do sed -i s/show_html_default=0/show_html_default=1/g $l
 done
